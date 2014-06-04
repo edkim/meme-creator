15d8af298bb1545ccf3d9a5614b5d8b5f1c5199d
Captain Deploy <ed@socialtables.com>
2014-06-04 01:34:16 -0400

    step 3

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 app.get('/', function(req, res){
-  res.send("hello world!")
+  res.send("<h1>hello world!</h1>");
 });
 
 app.listen(3000);
